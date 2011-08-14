d0158ee51cdc9b78e05a51c43e6617239541e8f1
Titainium Deng <knifewolf@gmail.com>
2011-08-14 21:35:26 +0800

    Add ie compatibility for html5

---
+++ static/js/ie_compat.js
@@ -0,0 +1,6 @@
+document.createElement("nav");
+document.createElement("header");
+document.createElement("footer");
+document.createElement("section");
+document.createElement("aside");
+document.createElement("article");
